199fc5fcd33af89dfdf180570d95dcd7881b727b
rsg73626 <renan.rsg@hotmail.com>
2018-06-21 20:44:31 -0300

    Arrumando problema 1018.

--- 1018 - Cédulas/cedulas.c
+++ 1018 - Cédulas/cedulas.c
@@ -4,14 +4,14 @@ int main()
 {
     int valores[] = {100, 50, 20 , 10 , 5, 2 , 1}, tamanho = 7,
     qtdValores[7] , entrada;
-    
+
     scanf("%d", &entrada);
-    
+    printf("%d\n", entrada);
     int i;
     for(i = 0; i < tamanho; i++){
         qtdValores[i] = entrada / valores[i];
         entrada = entrada % valores[i];
-        printf("%d nota(s) de RS %d,00\n", qtdValores[i], valores[i]);
+        printf("%d nota(s) de R$ %d,00\n", qtdValores[i], valores[i]);
     }
     return 0;
 }
