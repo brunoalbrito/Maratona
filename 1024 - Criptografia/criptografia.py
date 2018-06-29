6bc438bc97b248eb23073c9952d9b28ecd34e036
= <brunoalbuquerquebrito@outlook.com>
2018-06-29 18:19:27 -0300

    Resultido Exercicio 1024 - Criptografia

--- 1024 - Criptografia/criptografia.py
+++ 1024 - Criptografia/criptografia.py
@@ -1,14 +1,17 @@
 qtd = int(input())
-i = 0
-while(i < qtd):
+indice = 0
+while(indice < qtd):
   palavra = input()
-  resultado = []
-  k = 0
+  resultado = ""
+  meio = int(len(palavra) / 2)
+  i = 0
   for j in palavra:
-    resultado.append(ord(j) + 3)
-    k += 1
-  saida = ""
-  for j in resultado:
-    saida = saida + (chr(j))
+    if(j.isalpha()):
+      resultado = (chr(ord(j) + 3)) + resultado
+    else:
+      resultado = (chr(ord(j))) + resultado
+  saida = resultado[:meio]
+  for i in range(meio, len(resultado)):
+    saida = saida + chr(ord(resultado[i]) -1)
   print(saida)
-  i += 1
+  indice += 1
