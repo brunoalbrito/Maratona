9e3d8d52371552e44ef95d5c42e06dd100b054e8
= <brunoalbuquerquebrito@outlook.com>
2018-07-02 02:18:19 -0300

    Adicionando mes

---
+++ 1052 - Mes/mes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+mouth = ["January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December"]
+
+entrada = int(input())
+print(mouth[entrada -1])
